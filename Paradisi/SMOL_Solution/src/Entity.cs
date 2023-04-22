31b4410ad1d426335e874e414d8cce97e63e60a8
Giovanni Paradisi <giovanni.paradisi2@studio.unibo.it>
2023-04-22 14:33:18 +0200

    fix style

--- Paradisi/SMOL_Solution/src/Entity.cs
+++ Paradisi/SMOL_Solution/src/Entity.cs
@@ -1,12 +1,20 @@
 namespace MySpace;
-
-public class Entity
+///<summary>
+///The implementation of the Entity interface.
+///</summary>
+public class Entity : IEntity
 {
     private Type _entityType;
+
+    ///<summary>
+    ///Set the entity type.
+    ///</summary>
+    ///<param name = "type"></param>
     public Entity(Type type)
     {
         _entityType = type;
     }
 
+    ///<inheritdoc />
     public new Type GetType() => _entityType;
 }
---
+++ Paradisi/SMOL_Solution/src/IEntity.cs
@@ -0,0 +1,11 @@
+namespace MySpace;
+///<summary>
+///Entity interface.
+///</summary>
+public interface IEntity
+{
+    ///<summary>
+    ///Gets for the type of a entity.
+    ///</summary>
+    public Type GetType();
+}
